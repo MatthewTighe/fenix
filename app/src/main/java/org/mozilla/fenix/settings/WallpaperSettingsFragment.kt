a083c5ef2dc9aaddc1bd1ec3b4633091f9be5726
Matt Tighe <mtighe@mozilla.com>
2021-12-21 12:39:35 -0800

    switch to LazyVerticalGrid implementation

--- app/src/main/java/org/mozilla/fenix/settings/WallpaperSettingsFragment.kt
+++ app/src/main/java/org/mozilla/fenix/settings/WallpaperSettingsFragment.kt
@@ -8,18 +8,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Colors
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
@@ -44,7 +44,7 @@ class WallpaperSettingsFragment : Fragment() {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 FirefoxTheme {
-                    WallpaperSettings(wallpapers = Wallpaper.values().toList().dropLast(1))
+                    WallpaperSettings(wallpapers = Wallpaper.values().toList())
                 }
             }
         }
@@ -53,54 +53,111 @@ class WallpaperSettingsFragment : Fragment() {
 }
 
 enum class Wallpaper(val drawable: Int) {
+    NONE(R.attr.homeBackground),
     FIRST(R.drawable.wallpaper_1),
     SECOND(R.drawable.wallpaper_2),
-    NONE(R.attr.homeBackground);
+    THIRD(R.drawable.wallpaper_1);
 }
 
 @Composable
 fun WallpaperSettings(wallpapers: List<Wallpaper>) {
     var currentlySelectedWallpaper by remember { mutableStateOf(wallpapers[0]) }
-    Surface {
-        Column {
-            for (wallpaperRow in wallpapers.chunked(3)) {
-                Row{
-                    for (wallpaper in wallpaperRow) {
-                        val thumbnailShape = RoundedCornerShape(8.dp)
-                        val border = if (currentlySelectedWallpaper == wallpaper) {
-                            Modifier.border(BorderStroke(2.dp, FirefoxTheme.colors.borderSelected), thumbnailShape)
-                        } else {
-                            Modifier
-                        }
-                        Box(
-                            modifier = Modifier
-                                .height(102.dp)
-                                .width(92.dp)
-                                .padding(4.dp)
-                                .clip(thumbnailShape)
-                                .then(border)
-                        ) {
-                            Image(
-                                painterResource(id = wallpaper.drawable),
-                                contentScale = ContentScale.FillBounds,
-                                contentDescription = wallpaper.name,
-                                modifier = Modifier
-                                    .fillMaxHeight()
-                                    .fillMaxWidth()
-                                    .clickable {
-                                        currentlySelectedWallpaper = wallpaper
-                                    }
-                            )
-                        }
-                    }
-                }
-            }
+    Column {
+        Surface {
+            WallpaperThumbnails(
+                wallpapers = wallpapers,
+                onSelectionChanged = { currentlySelectedWallpaper = it },
+                selectedWallpaper = currentlySelectedWallpaper
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 24.dp)
+        ) {
+            var wallpapersEnabled by remember { mutableStateOf(false) }
+            Text("Logo wallpaper switch")
+            Switch(checked = wallpapersEnabled, onCheckedChange = { wallpapersEnabled = it })
         }
     }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun WallpaperThumbnails(
+    wallpapers: List<Wallpaper>,
+    onSelectionChanged: (Wallpaper) -> Unit,
+    selectedWallpaper: Wallpaper
+) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(3),
+        modifier = Modifier.padding(vertical = 30.dp, horizontal = 20.dp)
+    ) {
+        items(wallpapers) { wallpaper ->
+            WallpaperThumbnailItem(
+                wallpaper = wallpaper,
+                onSelectionChanged = onSelectionChanged,
+                isCurrentlySelected = selectedWallpaper == wallpaper
+            )
+        }
+    }
+}
+
+@Composable
+fun WallpaperThumbnailItem(
+    wallpaper: Wallpaper,
+    onSelectionChanged: (Wallpaper) -> Unit,
+    isCurrentlySelected: Boolean
+) {
+    val thumbnailShape = RoundedCornerShape(8.dp)
+    val border = if (isCurrentlySelected) {
+        Modifier.border(
+            BorderStroke(width = 2.dp, color = FirefoxTheme.colors.borderSelected),
+            thumbnailShape
+        )
+    } else {
+        Modifier.border(
+            BorderStroke(width = 1.dp, color = FirefoxTheme.colors.borderDivider),
+            thumbnailShape
+        )
+    }
+    val background = if (wallpaper == Wallpaper.NONE) {
+        Modifier.background(color = FirefoxTheme.colors.layer1)
+    } else {
+        Modifier
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1.1f)
+            .padding(4.dp)
+            .clip(thumbnailShape)
+            .then(background)
+            .then(border)
+            .clickable { onSelectionChanged(wallpaper) }
+    ) {
+        if (wallpaper != Wallpaper.NONE) {
+            WallpaperImageThumbnail(
+                wallpaper = wallpaper,
+            )
+        }
+    }
+}
+
+@Composable
+fun WallpaperImageThumbnail(
+    wallpaper: Wallpaper,
+) {
+    Image(
+        painterResource(id = wallpaper.drawable),
+        contentScale = ContentScale.FillBounds,
+        contentDescription = wallpaper.name,
+    )
 }
 
 @Preview
 @Composable
 fun WallpaperSettingsPreview() {
-    WallpaperSettings(wallpapers = Wallpaper.values().toList().dropLast(1))
+    WallpaperSettings(wallpapers = Wallpaper.values().toList())
 }
