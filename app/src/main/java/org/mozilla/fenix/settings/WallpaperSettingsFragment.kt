0dde24e121b8ee9fd941cfc59b0b70e7d4915998
Matt Tighe <mtighe@mozilla.com>
2021-12-22 10:28:53 -0800

    add localized string for image content description

--- app/src/main/java/org/mozilla/fenix/settings/WallpaperSettingsFragment.kt
+++ app/src/main/java/org/mozilla/fenix/settings/WallpaperSettingsFragment.kt
@@ -21,11 +21,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import org.mozilla.fenix.R
 import org.mozilla.fenix.databinding.FragmentWallpaperSettingsBinding
 import org.mozilla.fenix.ext.requireComponents
 import org.mozilla.fenix.theme.FirefoxTheme
@@ -140,10 +142,13 @@ fun WallpaperThumbnailItem(
 fun WallpaperImageThumbnail(
     wallpaper: Wallpaper,
 ) {
+    val localizedContentDescription = LocalContext.current.resources.getString(
+        R.string.content_description_wallpaper_name, wallpaper.name
+    )
     Image(
         painterResource(id = wallpaper.resource),
         contentScale = ContentScale.FillBounds,
-        contentDescription = wallpaper.name,
+        contentDescription = localizedContentDescription,
         modifier = Modifier.fillMaxSize()
     )
 }
